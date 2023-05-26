80c151fb784b36e4cf668c6d040be1c8d5076367
Viktoriia Veselova <viktoriia_veselova@epam.com>
2023-05-26 17:02:29 +0200

    initial commit

---
+++ src/main/java/com/lira17/expensetrackerbot/ExpenseTrackerBotApplication.java
@@ -0,0 +1,13 @@
+package com.lira17.expensetrackerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lira17/expensetrackerbot/ExpenseTrackerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lira17.expensetrackerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
